becd7eedd4660f3267fb46d0d8b0eef84fa17a60
bdrab <bartekdrab4@wp.pl>
2022-08-29 18:22:05 +0200

    filter option added

--- script.js
+++ script.js
@@ -18,10 +18,9 @@ class App{
     constructor(){
         addTaskBtn.addEventListener('click', this.addNewTask.bind(this))
         document.addEventListener('keypress', this.useEnter.bind(this))
-        tasksSelect.addEventListener('change', this.displayNewOrder)
+        tasksSelect.addEventListener('change', this.displayNewOrder.bind(this))
         toDoContainer.addEventListener('click', this.manageTask.bind(this))
         this.openTaskLocalStorage()
-        console.log(this.tasksArray)
     }
 
     manageTask(event){
@@ -31,18 +30,43 @@ class App{
     }
 
     toDoTaskButtonDone(row){
-        const element = document.querySelector(`.taskText .${row.classList[1]}`)
+        const taskID = row.classList[1]
+        const element = document.querySelector(`.taskText .${taskID}`)
+        this.tasksArray.forEach(e=>{
+            if(e.taskID === Number(taskID.split("-")[1]))
+                e.taskstate === "todo" ? e.taskstate = "done" : e.taskstate = "todo"
+        })
+
         element.classList.toggle("crossed-text")
         row.textContent === "Done" ? row.textContent = "Undone" : row.textContent = "Done"
+        this.saveTaskToLocalStorage(this.tasksArray)
+        console.log(this.tasksArray)
     }
 
     toDoTaskButtonDelete(row){
         const element = document.querySelector(`.toDoTask.${row}`)
+        const id = Number(row.split("-")[1])
+        let index
+        this.tasksArray.forEach(e=>{
+            if(e.taskID === id){
+                index = this.tasksArray.indexOf(e)
+            }
+        })
+
+        this.tasksArray.splice(index,1)
+        this.saveTaskToLocalStorage(this.tasksArray)
         element.remove()
     }
 
     displayNewOrder(event){
-        console.log("do nothing");
+        let newArray = []
+        if(event.target.value === "all"){
+            newArray = [...this.tasksArray].reverse()
+        }else{
+            newArray = [...this.tasksArray].filter(e => e.taskstate === event.target.value).reverse()
+        }
+        toDoContainer.innerHTML = ""
+        newArray.forEach(e => this.displayTask(e))
     }
 
     useEnter(event){
@@ -56,10 +80,10 @@ class App{
             <div class="toDoTaskRow">
                 <div class="taskBTNs">
                     <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-                    <button class="toDoTaskButtonDone task-${task.taskID}">Done</button>
+                    <button class="toDoTaskButtonDone task-${task.taskID}">${task.taskstate ==="done"? "Undone": "Done"}</button>
                 </div>
                 <div class="taskText"> 
-                    <div class="taskText task-${task.taskID}">${task.taskText}</div>
+                    <div class="taskText task-${task.taskID} ${task.taskstate ==="done"? "crossed-text": ""}">${task.taskText}</div>
                 </div>
                 <div class="taskBTNs">
                     <button class="toDoTaskButtonDelete task-${task.taskID}">Delete</button>
@@ -82,17 +106,17 @@ class App{
     }
 
     saveTaskToLocalStorage(tasks){
-        console.log(tasks)
         localStorage.setItem("tasks", JSON.stringify(tasks))
     }
 
     openTaskLocalStorage(){
         this.tasksArray = JSON.parse(localStorage.getItem('tasks')) || []
-        if (!this.tasksArray) return 
-        this.tasksArray.reverse().forEach(e => this.displayTask(e))
+        if (!this.tasksArray) return
+        const displayArray = [...this.tasksArray].reverse()
+        displayArray.forEach(e => {
+            this.displayTask(e)
+        })
     }
-
-
 }
 
 
